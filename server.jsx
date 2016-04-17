966944f4995cd29785a8e1ed74051d1bd2ec9468
Carroll Yu <cyu@commonsense.org>
2016-04-17 16:36:04 -0700

    remove handlebars/templating view layer

--- server.jsx
+++ server.jsx
@@ -2,13 +2,11 @@ import fs from 'fs'
 import path from 'path'
 import express from 'express'
 import bodyParser from 'body-parser'
-import exphbr from 'express-handlebars'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import Html from './dist/helpers/Html'
 
 var app = express();
-var handlebars = exphbr.create({extname: '.html'});
 var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 4000));
@@ -16,17 +14,8 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.engine('html', handlebars.engine);
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
-
-// Client-side rendering (only) of comments
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
 // Server-side and client-side rendering of comments
-app.get('/comments', function(req, res) {
+app.get('/', function(req, res) {
   fs.readFile(COMMENTS_FILE, function(err, data) {
     var containerData = {
       data: JSON.parse(data),
