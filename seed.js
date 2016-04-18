6041b7e86200fc31324c610d599d649ecce4fef7
Carroll Yu <cyu@commonsense.org>
2016-04-18 13:10:11 -0700

    code cleanse

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var data = require('./seed-data.js')
+var data = require('./seed-data')
 var COMMENTS_FILE = 'comments.json';
 
 fs.writeFile(COMMENTS_FILE, JSON.stringify(data.comments, null, 4));
